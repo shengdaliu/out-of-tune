c279cbeb73c411bcb215d73a4980372421feb8f2
Shengda Liu <liu.shengda@hotmail.com>
2020-03-07 23:43:24 +0100

    feat(components): add BaseNote component featuring basic C major scale note for testing material ui

---
+++ src/components/BaseNotes.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react'
+
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+
+import { Tonal, Scale, Progression, Mode, Chord, Key } from '@tonaljs/modules'
+
+// import MidiWriter from 'midi-writer-js'
+// import MidiPlayer from 'midi-player-js'
+
+import Tone from 'tone'
+
+// // create a synth and connect it to the master output (your speakers)
+const synth = new Tone.Synth().toMaster()
+
+// a 4 voice Synth
+const polySynth = new Tone.PolySynth(4, Tone.Synth).toMaster()
+
+function BaseNotes (props) {
+  function handlePlayNote (note, duration) {
+    console.log(`This note ${note} is played for ${duration}.`)
+    synth.triggerAttackRelease(note, duration)
+  }
+
+  return (
+    <div className={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          <React.Fragment>
+            <Grid item xs={3}>
+              <Paper style={{
+                textAlign: 'center',
+                color: 'grey',
+                padding: '20px'
+              }} onClick={() => { handlePlayNote("C4", "8n") }}>Note C4 8n</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper style={{
+                textAlign: 'center',
+                color: 'grey',
+                padding: '20px'
+              }} onClick={() => { handlePlayNote("D4", "8n") }}>Note D4 8n</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper style={{
+                textAlign: 'center',
+                color: 'grey',
+                padding: '20px'
+              }} onClick={() => { handlePlayNote("E4", "8n") }}>Note E4 8n</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper style={{
+                textAlign: 'center',
+                color: 'grey',
+                padding: '20px'
+              }} onClick={() => { handlePlayNote("F4", "8n") }}>Note F4 8n</Paper>
+            </Grid>
+          </React.Fragment>
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <React.Fragment>
+            <Grid item xs={3}>
+              <Paper style={{
+                textAlign: 'center',
+                color: 'grey',
+                padding: '20px'
+              }} onClick={() => { handlePlayNote("G4", "8n") }}>Note G4 8n</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper style={{
+                textAlign: 'center',
+                color: 'grey',
+                padding: '20px'
+              }} onClick={() => { handlePlayNote("A4", "8n") }}>Note A4 8n</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper style={{
+                textAlign: 'center',
+                color: 'grey',
+                padding: '20px'
+              }} onClick={() => { handlePlayNote("B4", "8n") }}>Note B4 8n</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper style={{
+                textAlign: 'center',
+                color: 'grey',
+                padding: '20px'
+              }} onClick={() => { handlePlayNote("C5", "8n") }}>Note C5 8n</Paper>
+            </Grid>
+          </React.Fragment>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default BaseNotes
