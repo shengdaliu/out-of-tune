65d9acd9e5e66ca52ce1087e660fe1c2c3767ad7
shengdaliu <liu.shengda@hotmail.com>
2021-01-08 10:42:20 +0100

    update(index): add some logs

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './containers/App'
 // import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
+console.log('test')
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
